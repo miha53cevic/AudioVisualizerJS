22306e2a2c110ab68dd68075ea4b34cd04d28caf
miha53cevic <44951515+miha53cevic@users.noreply.github.com>
2019-10-08 14:45:03 +0200

    add seperate Channel mode

--- fft.js
+++ fft.js
@@ -9,6 +9,8 @@ class fft {
         this.N = 16384;
         this.QUAD_SIZE = 8;
         this.freq_bin = [20, 60, 250, 500];
+
+        this.SplitChannels = false;
     }
 
     init(arrayBuffer) {
@@ -30,10 +32,24 @@ class fft {
             this.data = this.source.buffer.getChannelData(0);
             console.log(this.data);
 
+            // SplitChannels option
+            if (this.SplitChannels) {
+                if (this.source.buffer.getChannelData(1) == undefined) {
+                    this.SplitChannels = false;
+                } else {
+                    this.data2 = this.source.buffer.getChannelData(1);
+                    console.log(this.data2);
+                }
+            }
+
             // Start player
             this.audioPlayer.play();
+
+            console.log(this.N);
         });
     }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     render() {
 
@@ -87,6 +103,101 @@ class fft {
             }
         }
     }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    render2Channels() {
+
+        // Only if this.audioPlayer has started run
+        if (!this.audioPlayer.paused || this.audioPlayer.currentTime) {
+
+            const mark = Number.parseInt((this.audioPlayer.currentTime * this.source.buffer.sampleRate).toString());
+
+            let input1 = Array();
+            let input2 = Array();
+
+            // Fill in input
+            for (let i = 0; i < (this.N / 2); i++) {
+
+                let index = i + mark;
+
+                // Hamming window the input
+                let sample = this.data[index] * (0.54 - (0.46 * Math.cos(2.0 * Math.PI * (i / ((this.N - 1) * 1.0)))));
+
+                // Windowed sample / signal
+                input1.push(sample);
+            }
+
+            for (let i = 0; i < (this.N / 2); i++) {
+
+                let index = i + mark;
+
+                // Hamming window the input
+                let sample = this.data2[index] * (0.54 - (0.46 * Math.cos(2.0 * Math.PI * (i / ((this.N - 1) * 1.0)))));
+
+                // Windowed sample / signal
+                input2.push(sample);
+            }
+
+            // Calculate fft
+            const output1 = this.cfft(input1);
+            const output2 = this.cfft(input2);
+
+            let peakmaxArray1 = [];
+            let peakmaxArray2 = [];
+
+            // Calculate the magnitudes
+            for (let i = 0; i < (this.N / 4) + 1; i++) {
+
+                let freq = i * this.source.buffer.sampleRate / (this.N / 2);
+                let magnitude = output1[i].magnitude();
+
+                for (let j = 0; j < this.freq_bin.length - 1; j++) {
+                    if ((freq > this.freq_bin[j]) && (freq <= this.freq_bin[j + 1])) {
+                        peakmaxArray1.push(magnitude);
+                    }
+                }
+            }
+
+            for (let i = 0; i < (this.N / 4) + 1; i++) {
+
+                let freq = i * this.source.buffer.sampleRate / (this.N / 2);
+                let magnitude = output2[i].magnitude();
+
+                for (let j = 0; j < this.freq_bin.length - 1; j++) {
+                    if ((freq > this.freq_bin[j]) && (freq <= this.freq_bin[j + 1])) {
+                        peakmaxArray2.push(magnitude);
+                    }
+                }
+            }
+
+            // Clear screen
+            clear('rgb(51,51,51)');
+
+            // Visualize the magnitudes
+            for (let i = 0; i < peakmaxArray1.length; i++) {
+
+                const x = i * this.QUAD_SIZE + ((WIDTH / 2) - ((peakmaxArray1.length + peakmaxArray2.length) * this.QUAD_SIZE / 2));
+                const y = HEIGHT;
+
+                const height = peakmaxArray1[i] * 0.25;
+
+                drawFillRect(x, y, this.QUAD_SIZE, -height, 'red');
+            }
+
+            for (let i = peakmaxArray1.length; i < peakmaxArray1.length + peakmaxArray2.length; i++) {
+
+                const x = i * this.QUAD_SIZE + ((WIDTH / 2) - ((peakmaxArray1.length + peakmaxArray2.length) * this.QUAD_SIZE / 2));
+                const y = HEIGHT;
+
+                const height = peakmaxArray2[i - peakmaxArray1.length] * 0.25;
+
+                drawFillRect(x, y, this.QUAD_SIZE, -height, 'cyan');
+            }
+        }
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     cfft(amplitudes) {
         var N = amplitudes.length;
